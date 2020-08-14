5fb0df1bce45d356ab8116b90060aa85fe6e80f6
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-08-14 16:20:42 -0700

    seed successful

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
-const models = require('./seed.js');
+const models = require('./data/seed.js');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
