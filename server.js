71ec104246d0cd08d47740d8938f2dae748c8cb7
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-08-18 13:59:00 -0700

    moved card to modles folder and proceeding with hard coded data

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
-const { Card } = require('./index.js');
+const { Card } = require('./lib/models/Card.js');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
