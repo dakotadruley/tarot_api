8375c8926319bd3acdaf74f15f08a91574be2a4c
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-08-18 15:29:02 -0700

    decided to use parser

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,7 @@
+require('dotenv').config();
+// const Card = require('./lib/models/Card.js');
 
+const { tarotPage } = require('./lib/scraper/site.js');
+
+tarotPage()
+  .then(data => console.log(data));
