e90198b83d96da41e4eef9343a13583d0c6bc062
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-09-08 15:39:37 -0700

    added minor arcana

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 // const Card = require('./lib/models/Card.js');
 
-const { majorArcana, majorArcanaCard } = require('./lib/scraper/site.js');
+const {  minorArcana, minorArcanaCard } = require('./lib/scraper/site.js');
 
-majorArcana()
-  .then(urls => Promise.all(urls.map(url => majorArcanaCard(url))))
+// majorArcana()
+//   .then(urls => Promise.all(urls.map(url => majorArcanaCard(url))))
+//   .then(console.log);
+
+
+minorArcana().then(urls => Promise.all(urls.map(url => minorArcanaCard(url))))
   .then(console.log);
