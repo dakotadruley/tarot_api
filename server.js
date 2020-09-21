82a357ea190418ecd04e48d3abc6c7f8213afb8d
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-09-21 11:04:59 -0700

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT;
 const { Card } = require('./lib/models/Card.js');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
