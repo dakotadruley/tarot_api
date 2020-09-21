4cab7c9a1c30f7c309fd8175cd926ca1cf05a534
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-09-21 10:27:48 -0700

    updated port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
 const { Card } = require('./lib/models/Card.js');
 
 app.get('/', (req, res) => {
@@ -13,6 +13,6 @@ app.get('/card', async(req, res) => {
   res.json(cards);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Listening at ${PORT}`);
 });
